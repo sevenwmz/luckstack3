d14114ddf5c6663d9fd7310abbe43d76c8e038a3
月半子 王 <wangmingzhibj@163.com>
2020-04-12 20:23:01 +0800

    整理了内容和做了一些练习，【忽略这条更新】

---
+++ EntityTest/LinqMethodTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityTest
+{
+    class LinqMethodTest
+    {
+        [Test]
+        public void LinqMethod()
+        {
+            TestLinq.AssertLinq
+        }
+
+
+
+
+    }
+    public static class TestLinq
+    {
+        //public static void AssertLinq(this Assert assert, object[] expected, object[] actual)
+        public static bool AssertLinq<T>(List<object> expected, List<object> actual)
+        {
+            if (expected != actual)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+
+}
