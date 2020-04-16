ccb62955b990db0aeb33e22bb85d552a1fddf840
月半子 王 <wangmingzhibj@163.com>
2020-04-16 20:16:18 +0800

    [Finish] //重构之前的验证码作业：1.创建一个新的前台线程（Thread），在这个线程上运行生成随机字符串的代码 2.在一个任务（Task）中生成画布

--- Captcha/EmailSettle.cs
+++ Captcha/EmailSettle.cs
@@ -18,11 +18,11 @@ namespace Captcha
         /// Email生成随机数量
         /// </summary>
         /// <param name="value">生成的Email随机数量，不指定默认为300</param>
-        public static void MakerEail(int value =300)
+        public static void MakerEail(int value = 300)
         {
             //文件存放路径
             string path = @"C:\Users\Administrator\source\repos\luckstack3\Captcha\Email.txt";
-            
+
             //Email随机生成器
             using (StreamWriter writer = File.AppendText(path))
             {
@@ -34,12 +34,37 @@ namespace Captcha
                 }
                 writer.Flush();
             }
-            
+
         }
 
 
 
+        /// <summary>
+        /// 现有一个txt文件，里面存放了若干email地址，使用分号（;）或者换行进行了分隔。
+        /// 请删除其中重复的email地址，并按每30个email一行（行内用;分隔）重新组织
+        /// </summary>
+        public static void DeleteDuplicateEmail()
+        {
+            string path = @"C:\Users\Administrator\source\repos\luckstack3\Captcha\Email.txt";
 
+            string copyPath = @"C:\Users\Administrator\source\repos\luckstack3\Captcha\CopyEmail.txt";
+
+            ///需要先计算从0开始到；位置的字符串长度，然后去匹配，
+            ///先和一样长度的匹配，如果长度一样，在匹配所有位置的数字是否一样，一样就删除掉匹配字符串
+            ///一直删除到最后没有为止
+            ///
+            //File.Copy(path, copyPath);
+
+            //File.OpenRead(path);
+            //Distinct();
+        }
+
+        public static void SkipEmail()
+        {
+            string path = @"C:\Users\Administrator\source\repos\luckstack3\Captcha\Email.txt";
+            //File.OpenRead(path);
+            //FileStream fileStream = File.ReadAllText(path);
+        }
 
     }
 }
--- Captcha/ProgramNetFromWork.cs
+++ Captcha/ProgramNetFromWork.cs
@@ -4,14 +4,32 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Captcha
 {
     class ProgramNetFromWork
     {
+
+
         static void Main(string[] args)
         {
+
+
+            #region 2020.4.16异步作业
+            //重构之前的验证码作业：
+            //1.创建一个新的前台线程（Thread），在这个线程上运行生成随机字符串的代码
+            Thread thread = new Thread(new ThreadStart(Captcha.RefreshCaptcha));
+
+            //2.在一个任务（Task）中生成画布
+            Action draingMap = () =>
+            {
+                Bitmap image = new Bitmap(200, 100);
+                Graphics drawing = Graphics.FromImage(image);
+            };
+            Task task = new Task(draingMap);
+            #endregion
 
             #region MyRegion
             /// 现有一个txt文件，里面存放了若干email地址，使用分号（;）或者换行进行了分隔。
@@ -19,7 +37,7 @@ namespace Captcha
 
             //EmailSettle.MakerEail();
 
-
+            //EmailSettle.DeleteDuplicateEmail();
             #endregion
 
 
@@ -31,7 +49,7 @@ namespace Captcha
 
 
             //Captcha Exception Method
-            Captcha.CaptchaInsertException("52plk");
+            //Captcha.CaptchaInsertException("52plk");
             #endregion
 
 
