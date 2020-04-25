0297346e1aa9c24e923510c5baf4d513e39dfce4
月半子 王 <wangmingzhibj@163.com>
2020-04-25 20:49:41 +0800

    新画了一个NEW页面并和write一起添加了TagHelper和关键字（关键字循环的不是TagHelper不知道对不对）

--- Entity/Keyword.cs
+++ Entity/Keyword.cs
@@ -6,11 +6,20 @@ namespace Entity
 {
     public class Keyword
     {
-        public Keyword()
-        {
+        public int Id { set; get; }
 
-        }
         public string Name { get; set; }
+
         public IList<Article> articles { set; get; }
+
+        public IList<SecendKeyword> SecendKeyword { set; get; }
+
+        public IList<Keyword> SelfDefined { set; get; }
+    }
+    public class SecendKeyword
+    {
+        public string Name { set; get; }
+
+        public int Id { set; get; }
     }
 }
--- luckstack3/Pages/Profile/Write.cshtml.cs
+++ luckstack3/Pages/Profile/Write.cshtml.cs
@@ -2,16 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _17bang.Repository;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace luckstack3
 {
-    public class selfdescriptionModel : PageModel
+    public class Write : PageModel
     {
+
+        public bool? IsMale { set; get; }
+
+        public Constellation Constellation { set; get; }
+
+        public string Other { set; get; }
+
+        public IList<Keyword> Keywords { set; get; }
+        private KeywordRepository _keyword;
+        public Write()
+        {
+            _keyword = new KeywordRepository();
+        }
         public void OnGet()
         {
-
+            Keywords = _keyword.GetKeywords();
         }
+    }
+
+
+    public enum Constellation
+    {
+        白羊座,
+        金牛座,
+        双子座,
+        巨蟹座,
+        狮子座,
+        处女座,
+        天秤座,
+        天蝎座,
+        射手座,
+        摩羯座,
+        水瓶座,
+        双鱼座,
     }
 }
