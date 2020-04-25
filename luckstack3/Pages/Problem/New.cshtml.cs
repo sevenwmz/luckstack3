ef07ed59ac17ae34df4ee02b3b048ec6f6163018
月半子 王 <wangmingzhibj@163.com>
2020-04-25 21:26:15 +0800

    这个自动ignored了

---
+++ luckstack3/Pages/Problem/New.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using _17bang.Repository;
+
+namespace _17bang
+{
+    public class NewModel : PageModel
+    {
+        public string Title { set; get; }
+
+        public string Recommend { set; get; }
+
+        public string NameofSerch { set; get; }
+
+        public int RewardMoney { set; get; }
+
+        public string SelfKeywords { set; get; }
+        public IList<Keyword> Keywords { set; get; }
+        private KeywordRepository _keyword;
+        public NewModel()
+        {
+            _keyword = new KeywordRepository();
+        }
+
+        public void OnGet()
+        {
+            Keywords = _keyword.GetKeywords();
+        }
+    }
+}
