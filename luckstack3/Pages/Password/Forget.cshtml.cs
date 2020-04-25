0662dc350e416a6e8b30b43250d42b2a147deb7d
月半子 王 <wangmingzhibj@163.com>
2020-04-25 21:25:27 +0800

    修改了下午作业当中的TagHelper问题

--- luckstack3/Pages/Password/Forget.cshtml.cs
+++ luckstack3/Pages/Password/Forget.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,6 +13,7 @@ namespace luckstack3
 
         public string Email { set; get; }
 
+        public User UserName;
         public string Captcha { set; get; }
 
         public void OnGet()
