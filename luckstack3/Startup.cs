891373f571750cff404c493087c42c166b9252d6
月半子 王 <wangmingzhibj@163.com>
2020-04-29 13:56:43 +0800

    新增加了Article和Problem的Edit，和message/mine的页面

--- luckstack3/Startup.cs
+++ luckstack3/Startup.cs
@@ -30,7 +30,8 @@ namespace _17bang
                     option.Conventions
                         .AddPageRoute("/Article", "/Article/Page-{id:int}")
                         .AddPageRoute("/Problem","/Problem/Page-{id:int}")
-                        .AddPageRoute("/Suggest", "/Suggest/Page-{id:int}");
+                        .AddPageRoute("/Suggest", "/Suggest/Page-{id:int}")
+                        .AddPageRoute("/Message/Mine", "/Message/Mine/Page-{id:int}");
                 }).SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0); 
 
         }
