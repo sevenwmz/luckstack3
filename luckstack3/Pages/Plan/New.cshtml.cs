dd2faf196d675420bff1e26fef5b6ef656c6689d
月半子 王 <wangmingzhibj@163.com>
2020-05-01 16:14:27 +0800

    Plan/New验证改造完成

--- luckstack3/Pages/Plan/New.cshtml.cs
+++ luckstack3/Pages/Plan/New.cshtml.cs
@@ -2,29 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace _17bang
 {
+    [BindProperties]
     public class NewModelOfPlan : PageModel
     {
-        public string Title { set; get; }
-
-        public string Other { set; get; }
-
-        public string Tags { set; get; }
-
-        public int StartTime { set; get; }
-
-        public int EndTime { set; get; }
+        public Plan NewPlan { set; get; }
 
         public string[] WeekOfDay = new string[] { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
 
         public IList<bool> ChoosDay { set; get; }
 
-        public int Leave { set; get; }
 
         public IList<SelectListItem> LeaveRestDay { get; } =
          new List<SelectListItem>
@@ -37,13 +30,6 @@ namespace _17bang
                 new SelectListItem{Text= "6",Value= "6" },
          };
 
-
-        public string Evidence { set; get; }
-
-        public string WebAdress { set; get; }
-
-        public int Helper { set; get; }
-
         public IList<SelectListItem> HelperTotal { get; } =
          new List<SelectListItem>
          {
@@ -54,10 +40,7 @@ namespace _17bang
                 new SelectListItem{Text= "5",Value= "5" },
          };
 
-        public string Coin { set; get; }
-
-        public bool Continue { set; get; }
-
+ 
 
         public void OnGet()
         {
@@ -66,7 +49,10 @@ namespace _17bang
 
         public void OnPost()
         {
-
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
         }
     }
 }
