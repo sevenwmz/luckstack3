b9b66e2236ffdc3758c39397c1f8ad8290139f41
月半子 王 <wangmingzhibj@163.com>
2020-05-02 11:22:27 +0800

    Log/On的登陆验证加Cookie完成，需要redirect跳转

--- luckstack3/Pages/Log/on.cshtml.cs
+++ luckstack3/Pages/Log/on.cshtml.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using _17bang.ModelRepository.Log;
+using _17bang.Repository;
 using Entity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -13,12 +16,27 @@ namespace luckstack3
     public class SigninModel : PageModel
     {
 
-        public User UserName { set; get; }
+
+        [Required(ErrorMessage = "* 用户名不能为空")]
+        [StringLength(6, MinimumLength = 4, ErrorMessage = "* 最大长度不能超过6")]
+        [Display(Name = "* 用户名")]
+        public string Name { set; get; }
+
+        [DataType(DataType.Password)]
+        //[DisplayFormat(ConvertEmptyStringToNull = false)]
+        [Required(/*AllowEmptyStrings = true, */ErrorMessage = "* 密码不能为空")]
+        public string Password { set; get; }
+
+        private UserRepository _repository { set; get; }
+        public SigninModel()
+        {
+            _repository = new UserRepository();
+        }
 
         public bool RemenberMe { set; get; }
 
         public string Captcha { set; get; }
-
+        private int _cookie { set; get; }
         public void OnGet()
         {
 
@@ -30,6 +48,30 @@ namespace luckstack3
             {
                 return;
             }
+
+            User user = _repository.GetByName(Name);
+
+            if (user == null)
+            {
+                ModelState.AddModelError(nameof(Name), "用户名不存在");
+                return;
+            }
+            if (user.Password != Password)
+            {
+                ModelState.AddModelError(nameof(Password), "用户名或密码不正确");
+                return;
+            }
+
+
+            if (RemenberMe == true)
+            {
+                Response.Cookies.Append(Name, (++_cookie).ToString(),
+                        new CookieOptions { Expires = DateTime.Now.AddDays(90), });
+                return;
+            }
+
+            Response.Cookies.Append(Name, (++_cookie).ToString());
+
         }
     }
 }
