e9537c25cdcc507d51569b60e81f50f70d21c35b
月半子 王 <wangmingzhibj@163.com>
2020-05-03 21:50:28 +0800

    新增了1个页面去写LogOff的功能

---
+++ luckstack3/Pages/Log/Off.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17bang
+{
+    [BindProperties]
+    public class OffModel : PageModel
+    {
+        public User Name { set; get; }
+
+        public void OnGet()
+        {
+
+        }
+        public ActionResult OnPost()
+        {
+            if (Name != null)
+            {
+                Response.Cookies.Delete("wpzwpz");
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
