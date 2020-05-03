5ae4b154dd9eaa998636cec2184a1e6518c67af7
月半子 王 <wangmingzhibj@163.com>
2020-05-03 21:52:12 +0800

    完成LogOff的重定向功能，任何一个页面LogOff，都是刷新当前页面 退出登录，删除Cookie。

--- luckstack3/Pages/Log/Off.cshtml.cs
+++ luckstack3/Pages/Log/Off.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _17bang.Repository;
 using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,7 +12,11 @@ namespace _17bang
     [BindProperties]
     public class OffModel : PageModel
     {
-        public User Name { set; get; }
+        /// <summary>
+        /// 这里本意是获得当前的cookie用户，结果想不出来。
+        /// </summary>
+        public User Name;
+
 
         public void OnGet()
         {
@@ -19,10 +24,14 @@ namespace _17bang
         }
         public ActionResult OnPost()
         {
-            if (Name != null)
-            {
-                Response.Cookies.Delete("wpzwpz");
-            }
+
+            //if (Name != null)
+            //{
+            //    Response.Cookies.Delete(Name.ToString());
+            //}
+
+            //log/on我是用户名wpzwpz，Pwd 1234
+            Response.Cookies.Delete("wpzwpz");
 
             return RedirectToPage("/Index");
         }
