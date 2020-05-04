dd323471036453240b1c6ae5a8a0a2106d7b2c27
月半子 王 <wangmingzhibj@163.com>
2020-05-04 21:26:44 +0800

    Ad的View Component完成

--- luckstack3/Pages/Shared/_Ad.cshtml.cs
+++ luckstack3/Pages/Shared/_Ad.cshtml.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _17bang.Repository;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _17bang
 {
-    public class _AdModel : PageModel
+    public class _AdModel :ViewComponent
     {
-        public void OnGet()
-        {
+        public IList<Ad> Ad { set; get; }
+        private AdRepository _repository;
 
+        public _AdModel()
+        {
+            _repository = new AdRepository();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            Ad = _repository.Get();
+            return View("/Pages/Shared/_Ad.cshtml", Ad);
         }
     }
 }
--- luckstack3/Repository/AdRepository.cs
+++ luckstack3/Repository/AdRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,24 @@ namespace _17bang.Repository
 {
     public class AdRepository
     {
+        public static IList<Ad> _repository;
+
+        static AdRepository()
+        {
+            _repository = new List<Ad>
+            {
+                new Ad{ AdName = "学编程，来 “源栈”！飞哥精品小班等着你……" ,Id =1},
+                new Ad{ AdName = "免费广告位，抢到就是赚到！" ,Id = 2},
+                new Ad{ AdName = "免费广告位，抢到就是赚到！" ,Id = 3},
+                new Ad{ AdName = "免费广告位，抢到就是赚到！" ,Id = 4},
+                new Ad{ AdName = "免费广告位，抢到就是赚到！" ,Id = 5},
+                new Ad{ AdName = "本站主机由西部数码提供" ,Id = 6},
+            };
+        }
+        public IList<Ad> Get()
+        {
+            return _repository;
+        }
+
     }
 }
