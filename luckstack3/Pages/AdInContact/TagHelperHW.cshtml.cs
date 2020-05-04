43d19796d190658e594585661e6c5ee93b8d3a3c
月半子 王 <wangmingzhibj@163.com>
2020-05-05 07:36:34 +0800

    保存一些被Ignored的项目

---
+++ luckstack3/Pages/AdInContact/TagHelperHW.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17bang
+{
+    public class TagHelperHWModel : PageModel
+    {
+        public string Name { set; get; } = "wpz";
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ luckstack3/Repository/BaceRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _17bang.Repository
+{
+    public class BaceRepository<T>
+    {
+        public int GetSum(IList<T> name)
+        {
+            return name.Count;
+        }
+        public IList<T> Get(IList<T> name)
+        {
+            return name;
+        }
+        public void Add(T value, IList<T> name)
+        {
+            name.Add(value);
+        }
+
+    }
+}
