4224252eaf81aea843b838e42eaf0eb1557f7674
月半子 王 <wangmingzhibj@163.com>
2020-05-04 19:36:50 +0800

    新建一个_Keywords的partial View页面

---
+++ luckstack3/Pages/Shared/_Keywords.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _17bang.Repository;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17bang
+{
+    public class _KeywordsModel : ViewComponent
+    {
+        public IList<Keyword> Keyword { set; get; }
+        private KeywordRepository _repoisitory;
+
+        public _KeywordsModel()
+        {
+            _repoisitory = new KeywordRepository();
+        }
+
+        public IViewComponentResult Invoke(int max)
+        {
+            Keyword = _repoisitory.Get();
+            Keyword.Take(max).ToList();
+
+            return View();
+        }
+    }
+}
