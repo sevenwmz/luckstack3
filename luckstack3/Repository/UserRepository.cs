ed42e3abd644ba9557ba83dc4f64f4ba7f5c0b88
月半子 王 <wangmingzhibj@163.com>
2020-05-05 07:34:01 +0800

    添加了rank的level变色

--- luckstack3/Repository/UserRepository.cs
+++ luckstack3/Repository/UserRepository.cs
@@ -14,14 +14,14 @@ namespace _17bang.Repository
         {
             _users = new List<User>
             {
-                new User{UserName = "王胖子",Password = "1234",Level = "⑩",Integral = 999},
-                new User{UserName = "阿泰",Password = "1234",Level = "⑩",Integral = 499},
-                new User{UserName = "叶飞",Password = "1234",Level = "⑩",Integral = 929},
-                new User{UserName = "老刘",Password = "1234",Level = "⑩",Integral = 429},
-                new User{UserName = "欧阳",Password = "1234",Level = "⑩",Integral = 339},
-                new User{UserName = "wpz",Password = "1234",Level = "⑩",Integral = 339},
-                new User{UserName = "freefly",Password = "1234",Level = "⑩",Integral = 339},
-                new User{UserName = "老王",Password = "1234",Level = "⑩",Integral = 339},
+                new User{UserName = "王胖子",Password = "1234",Level = "9",Integral = 999},
+                new User{UserName = "阿泰",Password = "1234",Level = "8",Integral = 499},
+                new User{UserName = "叶飞",Password = "1234",Level = "3",Integral = 929},
+                new User{UserName = "老刘",Password = "1234",Level = "5",Integral = 429},
+                new User{UserName = "欧阳",Password = "1234",Level = "4",Integral = 339},
+                new User{UserName = "wpz",Password = "1234",Level = "7",Integral = 339},
+                new User{UserName = "freefly",Password = "1234",Level = "1",Integral = 339},
+                new User{UserName = "老王",Password = "1234",Level = "10",Integral = 339},
             };
         }
         public IList<User> Get()
