371ff3097429ed0c03f45a30f1536d78b5e71aeb
月半子 王 <wangmingzhibj@163.com>
2020-05-04 21:25:12 +0800

    增加了广告的类和数据

---
+++ Entity/Ad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class Ad
+    {
+        public int Id { set; get; }
+
+        public string AdName { set; get; }
+    }
+}
