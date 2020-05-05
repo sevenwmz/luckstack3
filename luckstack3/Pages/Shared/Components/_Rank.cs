e2ecb78d9b1b1098b7bcacca96390932056dd589
月半子 王 <wangmingzhibj@163.com>
2020-05-05 21:03:16 +0800

    重新修改了作业，移动了部分页的位置

---
+++ luckstack3/Pages/Shared/Components/_Rank.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Entity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using _17bang.Repository;
+
+namespace _17bang
+{
+    public class _RankModel : ViewComponent
+    {
+        public IList<User> UsersRank { set; get; }
+
+        private UserRepository _repository;
+
+        public _RankModel()
+        {
+            _repository = new UserRepository();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            UsersRank = _repository.Get();
+            return View("/Pages/Shared/_Rank.cshtml", UsersRank);
+        }
+    }
+}
