640ba33ce3445c50918a35ec1b4661001c6ce630
月半子 王 <wangmingzhibj@163.com>
2020-05-06 11:01:37 +0800

    完成了精品文章的partial view内容设置，添加到了problem里面，其他页面没有一一核对。

--- luckstack3/Pages/Shared/Components/_ExcellentArticle.cs
+++ luckstack3/Pages/Shared/Components/_ExcellentArticle.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _17bang.Pages.Repository;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace _17bang
 {
-    public class DefaultModel : PageModel
+    public class _ExcellentArticle : ViewComponent
     {
-        public void OnGet()
+        private ArticleRepository _repository;
+        public IList<Article> articles { set; get; }
+        public _ExcellentArticle()
         {
+            _repository = new ArticleRepository();
+        }
 
+        public IViewComponentResult Invoke(int max)
+        {
+            articles = _repository.Get().Take(max).ToList();
+
+            return View(articles);
         }
     }
 }
