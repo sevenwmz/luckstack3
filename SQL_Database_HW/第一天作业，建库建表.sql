486061c960dba0198c65668c7921749f37405210
月半子 王 <wangmingzhibj@163.com>
2020-05-07 15:07:08 +0800

    【Finish】新建一个数据库：17bang，能指定/查看该数据库存放位置

---
+++ SQL_Database_HW/第一天作业，建库建表.sql
@@ -0,0 +1,52 @@
+﻿Create Database [17bang];
+Go;
+Use [17bang]
+
+--2.观察“一起帮”的注册和发布求助功能，试着建立表User：包含字段UserName（用户名），Password（密码）…… 
+Create Table [dbo].[User]
+(
+[Id] Int Not Null Primary Key,
+[InviteName] Nchar(10) Not Null,
+[InviteNumber] int Not null,
+[User] Nchar(10) Not Null,
+[Password] Nchar(10) Not Null,
+--ConfirmPassword here don't need it. Cause,database just save data.
+--Same as verification code.
+);
+Go;
+
+Create Table [dbo].[Problem]
+(
+[Id] int Not Null Primary Key,
+[Title] Nchar(10) Not Null,
+[Body] Nchar(10) Not Null,
+[FristKeywords] Nchar(10) Not Null,
+[SecendKeywords] Nchar(10) Null,
+[SelfMadeKeywords] Nchar(10) Null,
+[DirectHelp] Nchar(10) Null,
+[Reward] Int Not Null
+);
+Go;
+
+Create Table [dbo].[FristKeywords]
+(
+[Id] Int Not Null Primary Key,
+[Keyword] Nchar(10) Not Null
+);
+Go;
+
+Create Table [dbo].[SecendKeywords]
+(
+[Id] Int Not Null Primary Key,
+[Keyword] Nchar(10) Not Null
+);
+Go;
+
+Create Table [dbo].[SelfMadeKeywords]
+(
+[UserId] Int Not Null Primary Key,
+[Id] Int Not Null,
+[Keyword] Nchar(10) Null
+);
+Go;
+
