1d69ee3813d3cc6a27b8852832a94fe50f4a0ad1
月半子 王 <wangmingzhibj@163.com>
2020-05-30 16:57:38 +0800

    [Article/New] page amost finish,Now can save article info and keywords,show exist AD and Serise.bit function do it later...like get user info from cookie.....(maby be never do it... )/xyx

---
+++ SQL_Database_HW/ArticleNewTest.sql
@@ -0,0 +1,141 @@
+﻿Create Table Series(
+	[Id] Int Primary Key Identity,
+	[Name] Nvarchar(50)
+)
+
+Alter Table Article
+Add SeriesId Int Constraint FK_Article_Series_Id Foreign Key References Series(Id)
+
+Select * From Article
+
+Insert Series(Name) Values(N'默认分类')
+Insert Series(Name) Values(N'讲课这些天')
+Insert Series(Name) Values(N'ASP.NET')
+Insert Series(Name) Values(N'Razor')
+Insert Series(Name) Values(N'CSharp')
+Insert Series(Name) Values(N'折腾')
+Insert Series(Name) Values(N'空系列')
+
+
+Select * From Article
+Alter Table Article
+Add AdId Int Constraint FK_Article_AD_Id Foreign Key References AD(Id)
+
+
+Insert AD(ContentOfAd) Values(N'----- 使用新的广告 -----')
+Insert AD(ContentOfAd) Values(N'微商宣传广告语 日加1000精准')
+Insert AD(ContentOfAd) Values(N'如果你听了一课之后发现不喜欢这门课程，那你可以要求退回你的学费，但必须用法语说。')
+Insert AD(ContentOfAd) Values(N'煮酒论英雄才子赢天下')
+
+Select * From AD
+
+
+
+Select ContentOfAd From AD
+
+
+Create Table AD(
+	[Id] Int Primary Key Identity,
+	[ContentOfAd] Nvarchar(255),
+	[Url] Nvarchar(1000)
+)
+
+Create Table Series(
+	[Id] Int Primary Key Identity,
+	[ContentOfSeries] Nvarchar(255),
+);
+Select Name,Id From Series
+
+Select * From Article Order By Id
+
+
+ALter Table Article
+Add Constraint PK_17bang_Article_Id Primary key(Id)
+
+Create Table KeywordToArticle(
+	[Id] Int Primary Key Identity,
+	[ArticleNameId] Int Constraint FK_Aritcle_Keyword_Id Foreign Key References Article(Id),
+	[KeywordId] Int Constraint FK_Keyword_Article_Id Foreign Key References Keyword(Id)
+);
+
+Select * From KeywordToArticle
+Select * From Keyword
+
+--Select [name] From Keyword Where N'123'  In (select [name] From Keyword)
+--if Select [name] From Keyword Where N'123' In (select [name] From Keyword)
+
+
+If (Select name From Keyword Where name = N'123') <> null
+Print 1
+
+Select * From Article
+Select * From KeywordToArticle
+--Insert KeywordToArticle(ArticleNameId,KeywordId) 
+--Values((Select id From Article Where Title = ),
+--(Select Id From Keyword Where [Name] = ) )
+--Update Keyword Set Used += 1 Where [Name] = 
+--Insert Keyword([Name],Used) Values()
+
+Begin Tran
+IF N'编程开发语言'  In (Select [name] From Keyword Where[name] = N'编程开发语言')
+Begin
+Insert KeywordToArticle(ArticleNameId, KeywordId)
+Values((Select Id From Article Where Title = N'王胖子的数据库' And AuthorId = (Select Id from [User] Where UserName = N'wpzwpz')),
+(Select Id From Keyword Where[Name] = N'编程开发语言'))
+Update Keyword Set Used += 1 Where [Name] = N'编程开发语言'
+End
+
+
+
+Rollback
+
+Begin Tran
+IF  N'编程开发语言'  In (Select [name] From Keyword Where[name] =  N'编程开发语言' ) 
+Begin 
+Insert KeywordToArticle(ArticleNameId, KeywordId) 
+Values(
+(Select Id From Article Where Title = N'我叫wpzKeywords' 
+And SUBSTRING(Content,0,25) = SUBSTRING(N'dasda',0,25)  
+And AuthorId = (Select Id from [User] Where UserName = N'wpzwpz')),
+
+(Select Id From Keyword Where[Name] = N'编程开发语言'))
+Update Keyword Set Used += 1 Where [Name] = N'编程开发语言'
+End
+ELSE 
+Insert Keyword([Name],Used) Values(N'编程开发语言',0) 
+
+
+Print @@TranCount
+
+
+ELSE
+Insert Keyword([Name],Used) Values(N'编程开发语言',0)
+
+
+
+
+Select * From Article
+
+Select * From Keyword
+
+
+IF N'Word' Not In (Select [name] From Keyword Where [name] = N'Word') 
+Print 1 else print 0
+Insert Keyword([Name]) Values(@KeywordPage)
+ELSE
+Begin
+Insert KeywordToArticle(ArticleNameId,KeywordId)
+Values((Select id From Article Where Title = @Title),
+(Select Id From Keyword Where[Name] = @KeywordPage))
+Update Keyword Set Used += 1 Where [Name] = @KeywordDatebase
+End
+
+Select * From KeywordToArticle
+Insert KeywordToArticle(ArticleNameId,KeywordId) 
+Values()
+
+
+Delete Article Where id between 98 and 117
+
+Delete Keyword Where id between 40 and 42
+
--- luckstack3/Pages/Article/New.cshtml.cs
+++ luckstack3/Pages/Article/New.cshtml.cs
@@ -15,46 +15,55 @@ using System.Data.SqlClient;
 
 namespace _17bang
 {
-    [NeedLogOn(role:"文章发布")]
+    [NeedLogOn(role: "文章发布")]
     public class NewOfArticleModel : PageModel
     {
 
-        #region BindPropretity
+        #region BindPropretity And Filed
+        [Required(ErrorMessage = "* 标题不能为空")]
         [BindProperty]
         public string Title { set; get; }
+
+        [Required(ErrorMessage = "* 内容不能为空")]
         [BindProperty]
         public string Content { set; get; }
 
         [Required(ErrorMessage = "系列不能为空")]
+        [BindProperty]
         public string Series { set; get; }
-        [BindProperty] 
+        [BindProperty]
         public string Summary { set; get; }
+        [BindProperty]
         public string Ad { set; get; }
+        [BindProperty]
         public string ContentOfAd { set; get; }
+        [BindProperty]
         public string WebSite { set; get; }
-        #endregion
+        [BindProperty]
+        public string Keywords { set; get; }
 
         public User UserInfo { set; get; }
-        public IList<Keyword> Keyword { set; get; }
+        //public IList<Keyword> Keyword { set; get; }
         //public Article NewArticle { set; get; }
 
         private ArticleRepository _repository { set; get; }
 
         public IList<SelectListItem> AdOfArticle { set; get; }
 
+        public IList<SelectListItem> SeriesOfSelect { set; get; }
+        //new List<SelectListItem>
+        //{
+        //       new SelectListItem{Text= "默认分类",Value= "1" },
+        //       new SelectListItem{Text= "讲课这些天",Value= "2" },
+        //       new SelectListItem{Text= "ASP.NET",Value= "3" },
+        //       new SelectListItem{Text= "Razor",Value= "4" },
+        //       new SelectListItem{Text= "CSharp",Value= "5" },
+        //       new SelectListItem{Text= "折腾",Value= "6" },
+        //       new SelectListItem{Text= "空系列",Value= "6" },
+        //};
+        #endregion
 
 
-        public IList<SelectListItem> SeriesOfSelect { get; } =
-         new List<SelectListItem>
-         {
-                new SelectListItem{Text= "默认分类",Value= "1" },
-                new SelectListItem{Text= "讲课这些天",Value= "2" },
-                new SelectListItem{Text= "ASP.NET",Value= "3" },
-                new SelectListItem{Text= "Razor",Value= "4" },
-                new SelectListItem{Text= "CSharp",Value= "5" },
-                new SelectListItem{Text= "折腾",Value= "6" },
-                new SelectListItem{Text= "空系列",Value= "6" },
-         };
         public NewOfArticleModel()
         {
             UserInfo = new User
@@ -66,29 +75,63 @@ namespace _17bang
                 Constellation = " 金牛座",
                 SelfIntroduction = "十年磨一剑，专注于ASP.NET网站开发"
             };
-            Keyword = new List<Keyword>
-            {
-                new Keyword{Name = "安装" },
-                new Keyword{Name = "ASP.NET" },
-                new Keyword{Name = "SQL" }
-            };
-            AdOfArticle = new List<SelectListItem>
-            {
-                new SelectListItem{Text= "----- 使用新的广告 -----",Value= "1" },
-                new SelectListItem{Text= "微商宣传广告语 日加1000精准",Value= "2" },
-                new SelectListItem{Text= "如果你听了一课之后发现不喜欢这门课程，那你可以要求退回你的学费，但必须用法语说。",Value= "3" },
-                new SelectListItem{Text= "煮酒论英雄才子赢天下",Value= "4" },
-            };
+            //Keyword = new List<Keyword>
+            //{
+            //    new Keyword{Name = "安装" },
+            //    new Keyword{Name = "ASP.NET" },
+            //    new Keyword{Name = "SQL" }
+            //};
+            //AdOfArticle = new List<SelectListItem>
+            //{
+            //    new SelectListItem{Text= "----- 使用新的广告 -----",Value= "1" },
+            //    new SelectListItem{Text= "微商宣传广告语 日加1000精准",Value= "2" },
+            //    new SelectListItem{Text= "如果你听了一课之后发现不喜欢这门课程，那你可以要求退回你的学费，但必须用法语说。",Value= "3" },
+            //    new SelectListItem{Text= "煮酒论英雄才子赢天下",Value= "4" },
+            //};
             _repository = new ArticleRepository();
         }
 
 
-
         public void OnGet()
         {
+            //Prepare for selectList
+            AdOfArticle = new List<SelectListItem> { };
+            SeriesOfSelect = new List<SelectListItem> { };
+
+            //Start connect database get selectList of AD and Series
+            string connectionToDatabase = "Data Source=-20191126PKLSWP;Initial Catalog=17bang;Integrated Security=True";
+            using (DbConnection connection = new SqlConnection(connectionToDatabase))
+            {
+                DbCommand sqlCommandOfAD = new SqlCommand("Select ContentOfAd,Id From AD", (SqlConnection)connection);
+                DbCommand sqlCommandOfSeries = new SqlCommand("Select Name,Id From Series", (SqlConnection)connection);
+
+                connection.Open();
+
+                //Take data of AD from datebase
+                SqlDataReader readerOfAD = (SqlDataReader)sqlCommandOfAD.ExecuteReader();
+                if (readerOfAD.HasRows)
+                {
+                    while (readerOfAD.Read())
+                    {
+                        AdOfArticle.Add(new SelectListItem { Text = readerOfAD["ContentOfAd"].ToString(), Value = readerOfAD["Id"].ToString() });
+                    }
+                }//else nothing
+
+                connection.Close();
+                connection.Open();
+
+                //Take data of Serise from datebase
+                SqlDataReader readerOfSeries = (SqlDataReader)sqlCommandOfSeries.ExecuteReader();
+                if (readerOfSeries.HasRows)
+                {
+                    while (readerOfSeries.Read())
+                    {
+                        SeriesOfSelect.Add(new SelectListItem { Text = readerOfSeries["Name"].ToString(), Value = readerOfSeries["Id"].ToString() });
+                    }
+                }//else nothing
+            }
 
         }
-
 
         public IActionResult OnPost()
         {
@@ -96,6 +139,8 @@ namespace _17bang
             {
                 return Redirect("/Article/New");
             }
+
+            #region Old Skill ADO.NET Of Connect Database
             //_repository.Save(NewArticle);
 
             //string connectionToDatabase = "Data Source=-20191126PKLSWP;Initial Catalog=17bang;Integrated Security=True";
@@ -111,35 +156,168 @@ namespace _17bang
 
             //    return Redirect("/Article");
             //}
+            #endregion
 
 
+            // If Summary is null ,take content in front of 255 char'.
+            if (string.IsNullOrEmpty(Summary))
+            {
+                Summary = Content.PadRight(255);
+            }
+
+
+
+            //Connect Database
             string connectionToDatabase = "Data Source=-20191126PKLSWP;Initial Catalog=17bang;Integrated Security=True";
             using (DbConnection connection = new SqlConnection(connectionToDatabase))
             {
+                #region Conver Keywords perpare to save 
+                //Get Keywords From Page,Prepare to Save
+                Keywords = Keywords.Trim();
+                IList<string> tempKeyworsFromPage = Keywords.Split(" ");
+
+                #endregion
+
                 connection.Open();
-                DbCommand sqlCommand = new SqlCommand(
-                    $"Insert Article(Title,Content,PublishTime,AuthorId) Values(@Title,@Content,GetDate()," +
-                    $"(Select u.Id From[User]u Where u.UserName = @Author))"
-                    );
 
+                #region DbParameters
                 //For Article Title
-                DbParameter pTitle = new SqlParameter("@Title",Title);
-
+                DbParameter pTitle = new SqlParameter("@Title", Title);
                 //For Article Content
                 DbParameter pContent = new SqlParameter("@Content", Content);
-                
                 //For Article Author
                 DbParameter pAuthor = new SqlParameter("@Author", UserInfo.NickName);
+                //For Article Summary
+                DbParameter pSummary = new SqlParameter("@Summary", Summary);
+                //For Article SeriesId
+                DbParameter pSeries = new SqlParameter("@SeriesId", Series);
+                #endregion
 
-                sqlCommand.Parameters.AddRange( new DbParameter[] { pTitle, pContent, pAuthor });
+                if (string.IsNullOrEmpty(WebSite))
+                {
+                    #region Save No Ad Article
+                    //For No AD Article
+                    using (
+                        DbCommand sqlCommandOfArticle = new SqlCommand(
+                        $"Insert Article(Title,Content,PublishTime,AuthorId,Summary,SeriesId) " +
+                        $"Values(" +
+                        $"@Title," +
+                        $"@Content," +
+                        $"GetDate()," +
+                        $"(Select u.Id From[User]u Where u.UserName = @Author)," +
+                        $"@Summary," +
+                        $"(Select Id From Series Where Id = @SeriesId))",
+                        (SqlConnection)connection)
+                        )
+                    {
+                        //Save To Article Database
+                        sqlCommandOfArticle.Parameters.AddRange(new DbParameter[] { pTitle, pContent, pAuthor, pSummary, pSeries });
+                        sqlCommandOfArticle.ExecuteNonQuery();
+                    }
+
+                    #endregion
+                }
+                else
+                {
+                    #region Save Ad
+                    //For AD
+                    using (DbCommand sqlCommandOfAD = new SqlCommand(
+                            $"Insert AD(ContentOfAd,Url) " +
+                            $"Values(@ContentOfAd,@WebSite)",
+                            (SqlConnection)connection))
+                    {
+                        //For Ad ContentOfAd 
+                        DbParameter pContentOfAd = new SqlParameter("@ContentOfAd", ContentOfAd);
+                        //For Ad WebSite
+                        DbParameter pWebSite = new SqlParameter("@WebSite", WebSite);
+                        //Save To Datebase
+                        sqlCommandOfAD.Parameters.AddRange(new DbParameter[] { pContentOfAd, pWebSite });
+                        sqlCommandOfAD.ExecuteNonQuery();
+                    }
+                    #endregion
+
+                    #region Save Has AD Article
+                    //Has AD Article
+                    using (
+                        DbCommand sqlCommandOfArticle = new SqlCommand(
+                            $"Insert Article(Title,Content,PublishTime,AuthorId,Summary,ADId,SeriesId) " +
+                            $"Values(" +
+                            $"@Title," +
+                            $"@Content," +
+                            $"GetDate()," +
+                            $"(Select u.Id From[User]u Where u.UserName = @Author)," +
+                            $"@Summary)," +
+                            $"(Select Id From AD Where ContentOfAd = @ContentOfAd)," +
+                            $"(Select Id From Series Where Name = @SeriesId)",
+                            (SqlConnection)connection)
+                        )
+                    {
+                        //Add ADId in Article
+                        DbParameter pContentOfAd = new SqlParameter("@ContentOfAd", ContentOfAd);
+                        sqlCommandOfArticle.Parameters.AddRange(new DbParameter[] { pTitle, pContent, pAuthor, pSummary, pContentOfAd, pSeries });
+                    }
+                    #endregion
+                }
+
+                #region Save Of Keyword
+                for (int i = 0; i < tempKeyworsFromPage.Count; i++)
+                {
+                    //Because Line 179 Can not use at here,make New one.  Guess something wrong?
+                    DbParameter pTitleUseByKeyword = new SqlParameter("@KTitle", Title);
 
 
-                sqlCommand.Connection = connection;
-                sqlCommand.ExecuteNonQuery();
+                    //Prepare Parameter
+                    DbParameter pKeywordPage = new SqlParameter("@KeywordPage", tempKeyworsFromPage[i]);
+                    using (
+                        DbCommand sqlCommandOfKeywords = new SqlCommand(
+                            //Check Keyword exist in Database
+                            $"IF N'{tempKeyworsFromPage[i]}' In (Select [name] From Keyword Where[name] = N'{tempKeyworsFromPage[i]}') " +
+                            $"Begin " +
+                            //Add To KeywordToArticle
+                            $"Insert KeywordToArticle(ArticleNameId, KeywordId) " +
+                            $"Values(" +
+                            $"(Select Id From Article Where Title = @KTitle " +
+
+                            //Use too much resoures ,But open check will be precisely
+                            //$"And SUBSTRING(Content,0,25) = SUBSTRING(N'{Content}',0,25) " +
+
+                            $"And AuthorId = (Select Id from [User] Where UserName = N'{UserInfo.NickName}')), " +
+
+                            $"(Select Id From Keyword Where[Name] = @KeywordPage)) " +
+                            $"Update Keyword Set Used += 1 Where [Name] = @KeywordPage " +
+                            $"End " +
+                            //
+                            //If this Keyword never appear ,add to keyword datebase,at same time add to n:n relation table
+                            //
+                            $"ELSE " +
+                            $"Insert Keyword([Name],Used) Values(@KeywordPage,0) " +
+                            $"Insert KeywordToArticle(ArticleNameId, KeywordId) " +
+                            $"Values(" +
+                            $"(Select Id From Article Where Title = @KTitle " +
+
+                            //Use too much resoures ,But open check will be precisely
+                            //$"And SUBSTRING(Content,0,25) = SUBSTRING(N'{Content}',0,25) " +
+
+                            $"And AuthorId = (Select Id from [User] Where UserName = N'{UserInfo.NickName}')), " +
+                            //For Scend KeywordToArticle parameter [KeywordId]
+                            $"(Select Id From Keyword Where[Name] = @KeywordPage)) ", (SqlConnection)connection
+                            )
+                        )
+                    {
+                        sqlCommandOfKeywords.Parameters.AddRange(new DbParameter[] { pTitleUseByKeyword, pKeywordPage });
+                        sqlCommandOfKeywords.ExecuteNonQuery();
+                    }
+                }
+
+                #endregion
+
             }
 
             return Redirect("/Article");
-        }
-    }
 
+
+        }
+
+    }
 }
+
