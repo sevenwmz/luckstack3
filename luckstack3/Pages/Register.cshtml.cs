0b14856f2d14b4d0e5b6034b7b9c2d330b86aff7
月半子 王 <wangmingzhibj@163.com>
2020-05-30 19:29:29 +0800

    Register save to database，finish

--- luckstack3/Pages/Register.cshtml.cs
+++ luckstack3/Pages/Register.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using _17bang;
@@ -14,13 +16,17 @@ namespace luckstack3
     [BindProperties]
     public class RegisterModel : PageModel
     {
-        [Required(ErrorMessage ="* 邀请人不能为空")]
+        [Required(ErrorMessage = "* 邀请人不能为空")]
         public string Inviter { set; get; }
 
         [Required(ErrorMessage = "* 邀请码不能为空")]
         public int? InviterNumber { set; get; }
 
-        public User Regester { get; set; }
+        public string UserName { set; get; }
+
+        public string Password { set; get; }
+
+        public string PasswordAgain { set; get; }
 
         public string Captcha { set; get; }
 
@@ -36,7 +42,52 @@ namespace luckstack3
             {
                 return Page();
             }
-            HttpContext.Session.SetString(Const.COOKIE_USER,Const.COOKIE_VALUE);
+
+            //Connect to database
+            string connectionToDatabase = "Data Source=-20191126PKLSWP;Initial Catalog=17bang;Integrated Security=True";
+            using (DbConnection connection = new SqlConnection(connectionToDatabase))
+            {
+                connection.Open();
+                #region Prepare parameter 
+                //For Invitername
+                DbParameter pInviteName = new SqlParameter("@Inviter", Inviter);
+                //For InviterNumber
+                DbParameter pInviteNumber = new SqlParameter("@InviterNumber", InviterNumber);
+                //For UserName
+                DbParameter pUserName = new SqlParameter("@UserName", UserName);
+                //For Password
+                DbParameter pPassword = new SqlParameter("@Password", Password);
+                #endregion
+
+                #region Save Register
+                using (
+                    DbCommand sqlRegister = new SqlCommand(
+                        //Add BMoney use for later.
+                        $"Declare @TempId Int = (Select Max(Id)+1 From HelpMoney) " +
+                        $"Insert HelpMoney(Id,BMoney,Detail) Values(@TempId,50,N'注册赠送50帮帮币') " +
+
+                        //Add new user.
+                        $"Insert[User](InviteName, InviteNumber, UserName,[Password], InviteById, BMoneyId)" +
+                        $"Values(" +
+                        $"(Select u.UserName From[User] u Where u.UserName = @Inviter )," +
+                        $"(Select u.InviteNumber From[User] u Where u.InviteNumber = @InviterNumber And u.UserName = @Inviter)," +
+                        $"@Username," +
+                        $"@Password," +
+                        $"(Select u.Id From[User] u Where u.InviteName = @Inviter)," +
+
+                        //Here I'm not sure about @@Identity.
+                        $"(Select Id From HelpMoney Where Id = @TempId) " +
+                        $") ", (SqlConnection)connection
+                        )
+                    )
+                {
+                    sqlRegister.Parameters.AddRange(new DbParameter[] { pInviteName, pInviteNumber, pUserName, pPassword });
+                    sqlRegister.ExecuteNonQuery();
+                }
+                #endregion
+            }
+
+            HttpContext.Session.SetString(Const.COOKIE_USER, Const.COOKIE_VALUE);
             Response.Cookies.Append(Const.COOKIE_USER, Const.COOKIE_VALUE);
             return Redirect(Request.Query[Const.PREPAGE]);
         }
