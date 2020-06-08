73a989d91bf56566084599f78fd1d6f656ea8164
月半子 王 <616309891@qq.com>
2020-06-08 20:06:16 +0800

    Make smple page about MoneyTrader Sale,HTML and BSTROOP

--- luckstack3/Pages/MoneyTrade/Sale.cshtml.cs
+++ luckstack3/Pages/MoneyTrade/Sale.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,10 @@ namespace _17bang
 {
     public class SaleModel : PageModel
     {
+        public User User { set; get; }
+        public int SaleCount { set; get; }
+        public int TotalSaleMoney { set; get; }
+        public string Message { set; get; }
         public void OnGet()
         {
 
