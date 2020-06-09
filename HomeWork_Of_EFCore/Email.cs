99cfe876b6769ef8f98e9976c376fdc82e6c66a9
月半子 王 <616309891@qq.com>
2020-06-09 17:27:34 +0800

    Delete database ,after Create new one

---
+++ HomeWork_Of_EFCore/Email.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_Of_EFCore
+{
+    public class Email : BaceEntity
+    {
+        public string EmailLocation { set; get; }
+        public string Remark { set; get; }
+    }
+}
---
+++ HomeWork_Of_EFCore/Migrations/20200609091541_AddDatabaseAndTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeWork_Of_EFCore.Migrations
+{
+    public partial class AddDatabaseAndTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Email",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmailLocation = table.Column<string>(nullable: true),
+                    Remark = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Email", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Register",
+                columns: table => new
+                {
+                    UserName = table.Column<string>(maxLength: 256, nullable: false),
+                    Id = table.Column<int>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    CreateTime = table.Column<DateTime>(nullable: false),
+                    SendToId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Register", x => x.UserName);
+                    table.ForeignKey(
+                        name: "FK_Register_Email_SendToId",
+                        column: x => x.SendToId,
+                        principalTable: "Email",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Register_CreateTime",
+                table: "Register",
+                column: "CreateTime")
+                .Annotation("SqlServer:Clustered", false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Register_UserName",
+                table: "Register",
+                column: "UserName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Register_SendToId",
+                table: "Register",
+                column: "SendToId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Register");
+
+            migrationBuilder.DropTable(
+                name: "Email");
+        }
+    }
+}
