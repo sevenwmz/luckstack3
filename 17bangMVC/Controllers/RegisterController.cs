603ae03d2e7f524423e7ad842d484f52864be61b
月半子 王 <616309891@qq.com>
2020-06-14 07:20:17 +0800

    Try to move project from core to mvc ,have lot of problem, now just a little bit finish view of register,without action result and repository.

--- 17bangMVC/Controllers/RegisterController.cs
+++ 17bangMVC/Controllers/RegisterController.cs
@@ -1,11 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Mvc;
 
 namespace _17bangMVC.Controllers
 {
-    public class RegisterController
+    public class RegisterController : Controller
     {
+
+        public ActionResult Index()
+        {
+            ViewData["preRegister"] = Request.QueryString["preRegister"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(int? Id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            //User user = new User
+            //{
+            //    Inviter = this.Inviter,
+            //    InviterNumber = this.InviterNumber,
+            //    UserName = this.UserName,
+            //    Password = this.Password
+            //};
+
+            #region Check Info Is Correct
+            //UserRepository repository = new UserRepository();
+            //if (!repository.CheckInvitedName(user.Inviter))
+            //{
+            //    ModelState.AddModelError(nameof(Inviter), "用邀请人不正确呦");
+            //    return Page();
+            //}//eles nothing
+            //if (!repository.CheckInviteCode(user.InviterNumber))
+            //{
+            //    ModelState.AddModelError(nameof(InviterNumber), "邀请码不正确呦");
+            //    return Page();
+            //}//eles nothing
+            //if (!repository.UserNameHasRepeat(user.UserName))
+            //{
+            //    ModelState.AddModelError(nameof(UserName), "用户名已存在呦，换一个吧");
+            //    return Page();
+            //}//eles nothing
+            #endregion
+
+            //_userRepository.Save(user);
+
+
+
+
+            //HttpContext.Session.SetString(Const.COOKIE_USER, Const.COOKIE_VALUE);
+            //Response.Cookies.Append(Const.COOKIE_USER, Const.COOKIE_VALUE);
+            //LogOnModel.IsLogIn = true;
+            //return Redirect(Request.Query[Const.PREPAGE]);
+            return View();
+        }
     }
 }
--- 17bangMVC/Models/RegisterModels.cs
+++ 17bangMVC/Models/RegisterModels.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace _17bangMVC.Models
 {
-    public class Register
+    public class RegisterModels
     {
+        #region Register preproty
+        [Display(Name = "邀请人：（* 必填")]
+        [Required(ErrorMessage = "* 邀请人不能为空")]
+        public string Inviter { set; get; }
+
+        [Display(Name = "邀请码：（* 必填）")]
+        [Required(ErrorMessage = "* 邀请码不能为空")]
+        public string InviterNumber { set; get; }
+
+        [Required]
+        [Display(Name = "用户名：（* 必填）")]
+        public string UserName { set; get; }
+
+        [Display(Name = "密码：（* 必填） ")]
+        public string Password { set; get; }
+
+        [Display(Name = "验证密码：（* 必填）")]
+        [Compare("Password")]
+        public string PasswordAgain { set; get; }
+
+        [Display(Name = "验证码：（* 必填） ")]
+        public string Captcha { set; get; }
+
+        public int CookieId { set; get; }
+
+        //private UserRepository _userRepository;
+        #endregion
+        public RegisterModels()
+        {
+            //_userRepository = new UserRepository();
+        }
     }
 }
