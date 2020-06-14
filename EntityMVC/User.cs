fd11f719af8848380e0ee00071577c4536d57e04
月半子 王 <616309891@qq.com>
2020-06-15 07:48:09 +0800

    New project.

---
+++ EntityMVC/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityMVC
+{
+    public class User
+    {
+        public int Id { set; get; }
+        public string Inviter { set; get; }
+        public string InviterNumber { set; get; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public int Level { set; get; }
+
+    }
+}
