685d011b7341350377f4e4983179bfbcfb3043c3
月半子 王 <616309891@qq.com>
2020-06-17 09:16:43 +0800

    add and move some .cs file to proparely position

---
+++ RepositoryMVC/Extension/BMoneyExtension.cs
@@ -0,0 +1,19 @@
+﻿using EntityMVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryMVC.Extension
+{
+    public static class BMoneyExtension
+    {
+        public static void AddNewRow(this BMoneyRepository bMoney , BMoney InsertInfo)
+        {
+            SqlContext _repository = new SqlContext();
+            _repository.BMoneys.Add(InsertInfo);
+            _repository.SaveChanges();
+        }
+    }
+}
---
+++ RepositoryMVC/SqlContext.cs
@@ -0,0 +1,27 @@
+﻿using EntityMVC;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryMVC
+{
+    public class SqlContext : DbContext
+    {
+        public DbSet<User> Users { set; get; }
+        public DbSet<BMoney> BMoneys { set; get; }
+
+
+        public SqlContext() : base("MvcRepository")
+        {
+#if DEBUG
+            Database.Log = s => File.AppendAllText("C:\\EF.log", s);
+#endif
+        }
+
+
+    }
+}
