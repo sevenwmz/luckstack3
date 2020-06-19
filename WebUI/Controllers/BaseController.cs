5cdefb2843feccf9147ae83992a220b20977c305
月半子 王 <616309891@qq.com>
2020-06-19 21:38:11 +0800

    most work finish,[change DbSet<T>]and[CurrentUserId]and[ContextPerRequest],but have issues

---
+++ WebUI/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Filter;
+
+namespace WebUI.Controllers
+{
+    [ContextPreRequest]
+    public class BaseController : Controller
+    {
+
+
+
+    }
+}
---
+++ WebUI/Filter/ContextPreRequest.cs
@@ -0,0 +1,21 @@
+﻿using ProductServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Filter
+{
+    public class ContextPreRequest : ActionFilterAttribute 
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (!filterContext.IsChildAction)
+            {
+                new BaceService().SaveChangesForFilter();
+            }
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
