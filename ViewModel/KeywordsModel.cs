f5dc40dfcb472431e5fdf5918617fba70c738dd8
月半子 王 <616309891@qq.com>
2020-06-20 17:51:30 +0800

    Add model use later

---
+++ ViewModel/KeywordsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    class KeywordsModel
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Used { set; get; }
+    }
+}
