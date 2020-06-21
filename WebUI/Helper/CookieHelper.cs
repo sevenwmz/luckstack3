e80dd870b333f586439d00e841a85180ab191500
月半子 王 <616309891@qq.com>
2020-06-21 17:29:39 +0800

    prepare to use

---
+++ WebUI/Helper/CookieHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.UIFunction
+{
+    public class CookieHelper
+    {
+        public static void AddCookie(int userId,string password,bool RemenberMe = false)
+        {
+            HttpCookie logIncookie = new HttpCookie(Const.COOKIE_NAME);
+            logIncookie.Values["id"] = userId.ToString();
+            logIncookie.Values["pwd"] = password;
+            if (RemenberMe)
+            {
+                logIncookie.Expires = DateTime.Now.AddDays(15);
+            }
+            HttpContext.Current.Response.Cookies.Add(logIncookie);
+        }
+    }
+}
