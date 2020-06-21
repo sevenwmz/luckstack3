55b7b3e8621ffac4e7332676aa31981a2aa5db49
月半子 王 <616309891@qq.com>
2020-06-21 14:38:30 +0800

    Add to ues

---
+++ WebUI/UIFunction/Cookie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUI.UIFunction
+{
+    public class Cookie
+    {
+        public void AddCookie(int userId,string password)
+        {
+            HttpCookie logIncookie = new HttpCookie(Const.COOKIE_NAME);
+            logIncookie.Values["id"] = userId.ToString();
+            logIncookie.Values["pwd"] = password;
+        }
+    }
+}
