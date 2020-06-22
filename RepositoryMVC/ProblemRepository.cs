2b1a43434da73d0fab4ded7c24e3a2c7c827c93b
月半子 王 <616309891@qq.com>
2020-06-22 19:20:18 +0800

    Basic problem/new page can run , without two of dropdown keywordlist

--- RepositoryMVC/ProblemRepository.cs
+++ RepositoryMVC/ProblemRepository.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using EntityMVC;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RepositoryMVC
 {
-    class ProblemRepository
+    public class ProblemRepository : BaceRepository<Problem>
     {
+        public ProblemRepository(DbContext contenxt) : base(contenxt)
+        {
+
+        }
     }
 }
--- WebUI/Controllers/ProblemController.cs
+++ WebUI/Controllers/ProblemController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ViewModel.Problem;
 
 namespace WebUI.Controllers
 {
     public class ProblemController : Controller
     {
         // GET: Problem
-        public ActionResult Index()
+        public ActionResult New()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult New(ProblemNewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+
+
+
+
+
             return View();
         }
     }
