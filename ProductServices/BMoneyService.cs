deac5a6a8736d87623e9e8096129c5697cb7c7e8
月半子 王 <616309891@qq.com>
2020-06-22 20:49:11 +0800

    Add to use

---
+++ ProductServices/BMoneyService.cs
@@ -0,0 +1,27 @@
+﻿using EntityMVC;
+using RepositoryMVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductServices
+{
+    public class BMoneyService : BaceService
+    {
+
+        private BMoney _bMoney;
+        private BMoneyRepository _repository;
+        public BMoneyService()
+        {
+            _bMoney = new BMoney();
+            _repository = new BMoneyRepository(dbContext);
+        }
+        internal void PublishProblem(int rewardMoney)
+        {
+            _bMoney = _bMoney.PublicProblemMinusBMoney(rewardMoney, _repository.GetByAuthorBMoney(CurrentUserId));
+            _repository.AddNewRow(_bMoney);
+        }
+    }
+}
