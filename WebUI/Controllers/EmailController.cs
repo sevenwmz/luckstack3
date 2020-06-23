46b9e84c8356f72af296403782623ef149479910
月半子 王 <616309891@qq.com>
2020-06-23 13:20:51 +0800

    Secend Email use

---
+++ WebUI/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Activate()
+        {
+            return View();
+        }
+    }
+}
