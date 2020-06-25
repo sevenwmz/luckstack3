f79f4ef282431bf3dda924952a41b2323c009a67
月半子 王 <616309891@qq.com>
2020-06-25 09:13:19 +0800

    add to use

---
+++ RepositoryMVC/KeywordAndProblemRepository.cs
@@ -0,0 +1,29 @@
+﻿using EntityMVC;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryMVC
+{
+    public class KeywordAndProblemRepository : BaceRepository<KeywordsAndProblem>
+    {
+        public KeywordAndProblemRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public IList<KeywordsAndProblem> GetKeywords(int id)
+        {
+            return entities.Include(k => k.Keyword).Where(k => k.ProblemId == id).ToList();
+        }
+
+        public void SaveToMiddle(int id, int keywordId)
+        {
+            entities.Add(new KeywordsAndProblem { ProblemId = id , KeywordId = keywordId });
+            context.SaveChanges();
+        }
+    }
+}
