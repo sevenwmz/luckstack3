6b5a631da25d16f3c4876063d8e8de3df2a562fa
月半子 王 <616309891@qq.com>
2020-06-25 09:14:42 +0800

    Ignore file

---
+++ ProductServices/KeywordAndProblemService.cs
@@ -0,0 +1,23 @@
+﻿using EntityMVC;
+using RepositoryMVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductServices
+{
+    public class KeywordAndProblemService : BaceService
+    {
+        internal void SaveMiddleTale(int id, string needSubKeyword)
+        {
+            IList<Keywords> keywords = new Keywords().GetKeywordList(needSubKeyword);
+            foreach (var item in keywords)
+            {
+                new KeywordAndProblemRepository(dbContext).SaveToMiddle(id, new KeywordRepository(dbContext).Find(item.Name));
+            }
+
+        }
+    }
+}
