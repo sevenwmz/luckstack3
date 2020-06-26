329a86d6c90d2a269c828fd144c3543c6e04b1c2
月半子 王 <616309891@qq.com>
2020-06-26 08:29:22 +0800

    prepare to use

---
+++ ViewModel/Article/ArticleSingle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Article
+{
+    public class ArticleSingle
+    {
+        public int Id { set; get; }
+        public string Author { set; get; }
+
+        public string Title { set; get; }
+
+        public string Body { set; get; }
+
+        public DateTime PublishTime { get; set; }
+
+        public string ChoosSeries { set; get; }
+
+        public IList<KeywordModel> Keywords { set; get; }
+    }
+}
---
+++ ViewModel/Article/ArticleSingleModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Article
+{
+    public class ArticleSingleModel
+    {
+        public int Id { set; get; }
+        public string Author { set; get; }
+
+        public string Title { set; get; }
+
+        public string Body { set; get; }
+
+        public DateTime PublishTime { get; set; }
+
+        public string ChoosSeries { set; get; }
+
+        public IList<KeywordModel> Keywords { set; get; }
+
+        public IList<string> Comments { set; get; }
+
+    }
+}
