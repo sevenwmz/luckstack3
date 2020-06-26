b3df83b881a77ddc7159f6dce9653e620e37a155
月半子 王 <616309891@qq.com>
2020-06-26 20:50:23 +0800

    prepare to use

---
+++ ViewModel/ChildAction/ChildRankModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.ChildAction
+{
+    public class ChildRankModel
+    {
+        public int Id { set; get; }
+        public string UserName { set; get; }
+        public int Level { set; get; }
+        public int BMoney { set; get; }
+
+        public List<ChildRankModel> Items { set; get; }
+
+    }
+}
