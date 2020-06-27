b075225c01852a5258de87863993c539fed40a3a
月半子 王 <616309891@qq.com>
2020-06-27 11:15:51 +0800

    prepare to use

---
+++ WebUI/Controllers/MessageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Mine()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ WebUI/Helper/DescriptionHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace WebUI.Helper
+{
+    public static class DescriptionHelper
+    {
+        public static string GetDescription<T>(this T status)
+        {
+            Type enumType = typeof(T);
+            FieldInfo enumFieldInfo = enumType.GetField(status.ToString());
+            DescriptionAttribute attribute = (DescriptionAttribute)
+                DescriptionAttribute.GetCustomAttribute(enumFieldInfo, typeof(DescriptionAttribute));
+
+            return attribute.Description;
+        }
+
+
+    }
+}
