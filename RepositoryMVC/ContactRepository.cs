1fe649f860ac0d2e43692f840207360f571e6ae5
月半子 王 <616309891@qq.com>
2020-06-27 17:30:01 +0800

    Prepare to use

---
+++ RepositoryMVC/ContactRepository.cs
@@ -0,0 +1,20 @@
+﻿using EntityMVC;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryMVC
+{
+    public class ContactRepository : BaceRepository<Contact>
+    {
+        public ContactRepository(DbContext context) : base(context)
+        {
+        }
+
+
+
+    }
+}
---
+++ ViewModel/Contact/ContactModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Contact
+{
+    public class ContactModel
+    {
+        public string QQ { set; get; }
+        public string WeChat { set; get; }
+        public int CellPhone { set; get; }
+        public string OtherMark { set; get; }
+
+    }
+}
