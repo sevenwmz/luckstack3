dd0a42b62bd255c8226ebc8dbb4a67ed2859bda2
月半子 王 <616309891@qq.com>
2020-06-27 21:34:58 +0800

    prepare to use

---
+++ ProductServices/ChangePasswordService.cs
@@ -0,0 +1,36 @@
+﻿using EntityMVC;
+using RepositoryMVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductServices
+{
+    public class ChangePasswordService : BaceService
+    {
+        /// <summary>
+        /// From DB take userInfo 
+        /// </summary>
+        /// <returns>Return currentUser pwd</returns>
+        public string GetUserPassword()
+        {
+            return new UserRepository(dbContext).Find(CurrentUserId).Password;
+        }
+
+        public void AddNewPwd(string newPassword)
+        {
+            try
+            {
+                connectedMapper.Map(
+                   newPassword, new UserRepository(dbContext).Find(CurrentUserId).Password);
+            }
+            catch (Exception)
+            {
+                //fake to save to file...
+                throw;
+            }
+        }
+    }
+}
---
+++ ViewModel/Password/ChangeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Password
+{
+    public class ChangeModel
+    {
+        public string OldPwd { set; get; }
+        public string NewPwd { set; get; }
+        public string ConfrimPwd { set; get; }
+    }
+}
