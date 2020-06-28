3dfaba42587b8fdd26c2ceb016d7544d72950159
月半子 王 <616309891@qq.com>
2020-06-28 10:34:39 +0800

    Prepare to use

---
+++ ViewModel/Password/ForgetModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Password
+{
+    public class Forget
+    {
+        public string EmailAddress { set; get; }
+        public string UserName { set; get; }
+
+    }
+}
