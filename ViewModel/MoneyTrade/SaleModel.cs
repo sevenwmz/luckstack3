aaa55c42a13eb05d1171bf66e57591fe912961a6
月半子 王 <616309891@qq.com>
2020-06-28 16:23:04 +0800

    prepare to use

---
+++ ViewModel/MoneyTrade/SaleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.MoneyTrade
+{
+    public class Sale
+    {
+        public int LeftBMoney { set; get; }
+        public int CanSale { set; get; }
+        public string ByFrom { set; get; }
+        public int SaleCount { set; get; }
+        public int TotalSaleMoney { set; get; }
+        public string Message { set; get; }
+
+    }
+}
