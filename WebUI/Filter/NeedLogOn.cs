5dcc297e5bbdc542f6ad0172ec5b5addf52d9323
月半子 王 <616309891@qq.com>
2020-07-01 20:20:55 +0800

    Add [Need log on] without test. move from rezor page

---
+++ WebUI/Filter/NeedLogOn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Filter
+{
+    public class NeedLogOnAttribute : ActionFilterAttribute
+    {
+
+        private string _role;
+        public NeedLogOnAttribute(string role)
+        {
+            _role = role;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            HttpContextBase httpContext = filterContext.HttpContext;
+            HttpRequestBase request = httpContext.Request;
+
+            string prePage = request.Path + request.QueryString;
+
+            string LogOnUser = httpContext.Session.GetString(Const.SESSION_USER);
+            if (string.IsNullOrEmpty(LogOnUser))
+            {
+                context.Result = new RedirectResult($"/Log/On?{Const.PREPAGE}={prePage}&{Const.ROLE}={_role}");
+            }//else nothing..
+
+
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
