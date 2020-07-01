24b47fe807d504120545c57385eb576de0aedb94
月半子 王 <616309891@qq.com>
2020-07-01 19:06:54 +0800

    Finish [JS] Homework,fist day. declear const and alter,and scend day

---
+++ luckstack3/wwwroot/js/SevenMarkJS.js
@@ -0,0 +1,27 @@
+﻿var sname = "wpz";
+const password = 1234;
+var inLineTolk = "编程";
+inLineTolk = "zhi ye";
+inLineTolk = "ren sheng";
+var problem = {
+    title: "new title",
+    body: "fake here is body",
+    reward: 100,
+    needRemote: true
+}
+
+//var problem = {};
+//problem.title = "new title";
+//problem.title = "new title";
+//problem.body = "fake here is body";
+//problem.reward = 100;
+//problem.needRemote = true;
+
+
+alert("Something comeing true");
+alert(sname);
+sname = "Li zhi bo";
+alert(sname);
+sname = "a tai";
+alert(sname);
+
