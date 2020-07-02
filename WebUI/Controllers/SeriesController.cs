7f61e92b148a10fc5213cf260eb874989465d7cc
月半子 王 <616309891@qq.com>
2020-07-02 17:47:49 +0800

    Prepare to use

---
+++ WebUI/Controllers/SeriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class SeriesController : Controller
+    {
+        // GET: Series
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
