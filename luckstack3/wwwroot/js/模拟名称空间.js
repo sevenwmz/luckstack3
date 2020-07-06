b9a3f8a4b006fc2928095e853b1a35f82ab9fa66
月半子 王 <616309891@qq.com>
2020-07-06 20:33:15 +0800

    Finish [(fake)nameSpace];

--- luckstack3/wwwroot/js/模拟名称空间.js
+++ luckstack3/wwwroot/js/模拟名称空间.js
@@ -1,6 +1,49 @@
-﻿//作业
+﻿////作业
+////使用“模拟名称空间”技术，构建一个函数函数yz.fei.get986(number) ；
+////在yz.fei.get986(number)中包含不带参数的函数has9() / has8() / has6() ；
+function myfunction() {
+    //function has9(number, findNum) {
+
+//    if (findNum == undefined) {
+//        findNum = 9;
+//    }
+
+//    var temp = `${number}`;
+//    for (var i = 0; i < temp.length; i++) {
+//        if (temp[i] == findNum) {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+
+    //function has8(number) {
+//    return has9(number, 8);
+//}
+
+//function has6(number) {
+//    return has9(number, 6);
+//}
 
 
-//使用“模拟名称空间”技术，构建一个函数函数yz.fei.get986(number) ；
+        //var yz = {};
+    //yz.fei = {
+    //    get986 : function (number) {
+    //    var count9 = 0,
+    //        count8 = 0,
+    //        count6 = 0;
+    //    var temp = `${number}`;
 
-//在yz.fei.get986(number)中包含不带参数的函数has9() / has8() / has6() ；
+    //    for (var i = 0; i < temp; i++) {
+    //        has9(i) ? count9++ : 0;
+    //        has8(i) ? count8++ : 0;
+    //        has6(i) ? count6++ : 0;
+    //    }
+    //    console.log(`In ${temp} have ${count9} - 9,and have ${count8} - 8, and ${count6} - 6`);
+    //    var arr = [count9, count8, count6];
+    //    return arr;
+    //}
+    //};
+
+    //yz.fei.get986(number);
+}
