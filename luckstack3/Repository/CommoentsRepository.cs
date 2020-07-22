f357e5cb5c1f61bf7149f5187cc87bc22ebda9e4
月半子 王 <616309891@qq.com>
2020-07-22 11:00:40 +0800

    Prepare to use

---
+++ luckstack3/Repository/CommoentsRepository.cs
@@ -0,0 +1,40 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _17bang.Repository
+{
+    public class CommoentsRepository
+    {
+        private DBHelper _helper;
+
+      
+        public IList<Ad> Get()
+        {
+            using (DbConnection connection = _helper.Connection)
+            {
+                IList<Ad> result = new List<Ad> { };
+                using (DbCommand cmd = new SqlCommand("Select ContentOfAd,Id From AD", (SqlConnection)connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = (SqlDataReader)cmd.ExecuteReader();
+                    if (!reader.HasRows)
+                    {
+                        return result;
+                    }
+                    while (reader.Read())
+                    {
+                        result.Add(new Ad { AdName = reader["ContentOfAd"].ToString(), Id = Convert.ToInt32(reader["Id"]) });
+                    }
+                    return result;
+                }
+            }
+        }
+
+
+    }
+}
