9e1c8de0609e5aee5f68a841c530758e2fbf80c2
月半子 王 <616309891@qq.com>
2020-07-22 21:45:52 +0800

    Prepare to use

---
+++ WebUI/Controllers/LogOnController.cs
@@ -0,0 +1,30 @@
+﻿using ProductServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class LogOnController : BaseController
+    {
+        // GET: _LogOn
+        [ChildActionOnly]
+        public ActionResult LogOn()
+        {
+            return View(new BaceService().CurrentUserId);
+        }
+
+
+
+        //[HttpPost]
+        //public PartialViewResult LogOn()
+        //{
+
+
+        //    return PartialView();
+        //}
+
+    }
+}
