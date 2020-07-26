c4630e479845b2bef72bcbda7d84d0cdbb208047
月半子 王 <616309891@qq.com>
2020-07-26 14:36:46 +0800

    Prepare to use

---
+++ EntityMVC/Chat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityMVC
+{
+    public class Chat : BaceEntity
+    {
+        public DateTime PublishTime { set; get; }
+
+        public int? ChatWithId { set; get; }
+        public User ChatWith { set; get; }
+
+        public int ChatUserId{ set; get; }
+        public User ChatUser { set; get; }
+
+        public string Content { set; get; }
+    }
+}
---
+++ ViewModel/ChildAction/ChatRoomModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.ChildAction
+{
+    public class ChatRoomModel
+    {
+        public int Id { set; get; }
+        public DateTime PublishTime { set; get; }
+        public string User { set; get; }
+        public int Level { set; get; }
+        public string MyChatContent { set; get; }
+
+
+    }
+}
