0ce556b3ecac6c62db4612d5dab1adb4a67498bf
月半子 王 <616309891@qq.com>
2020-07-29 21:32:59 +0800

    prepare to use

---
+++ ViewModel/ChildAction/ChildCommentModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.ChildAction
+{
+    public class ChildCommentModel
+    {
+        public int? CurrentId { set; get; }
+        public IList<ChildCommentItem> Comments { set; get; }
+    }
+
+
+    public class ChildCommentItem
+    {
+        public int Id { set; get; }
+        public DateTime PublishTime { set; get; }
+        public UserModel Author { set; get; }
+        public string Comment { set; get; }
+        public ChildCommentItem Reply { set; get; }
+        public int Agree { set; get; }
+        public int DisAgree { set; get; }
+    }
+}
