415c866c0e233bb837280454996e038ad9b6d207
月半子 王 <616309891@qq.com>
2020-07-30 21:39:23 +0800

    Prepare to use

---
+++ RepositoryMVC/CommentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using EntityMVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryMVC
+{
+    public class CommentsRepository : BaceRepository<Comments>
+    {
+        public CommentsRepository(SqlContext context) : base(context)
+        {
+
+        }
+
+        public int SaveComment(Comments commentModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
