7730b4cc8968e3bd981f60947e50b4f7378444f0
月半子 王 <616309891@qq.com>
2020-08-02 21:54:47 +0800

    Finish [ShowKeyword] at Problem,not finish,remove have some problem

---
+++ WebUI/Scripts/MyJavaScript/KeywordRefresh.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+$(document).ready(function () {
+    $('[js-fristKeyword]').children().click(function () {
+        let $this = $(this),
+            $stuffKeyword = $('[js-checkedKeyword]');
+
+        let addCheckedKeyword = $(
+            `<span js-showCheckedKeyword class="badge badge-has-used" style="margin-right:5px;">${$this.text()}
+                <small js-remove style = "margin-left:5px;" class="fa fa-times"></small >
+            </span >`);
+        $stuffKeyword.prepend(addCheckedKeyword);
+        $stuffKeyword.css('display', '');
+
+        $('[js-checkedKeywordId]').attr('id', $this.attr('id'));
+    });
+
+    $('[js-remove]').click(function () {
+        let $showKeywords = $('[js-checkedKeyword]');
+
+        $(this).remove();
+        if ($showKeywords.children().length === 0) {
+            $showKeywords.css('display', 'none');
+        }
+    });
+
+
+
+});
+
+
+function errorFedback(a, b, c) {
+    console.log('Has some problem now!!!');
+}
+
