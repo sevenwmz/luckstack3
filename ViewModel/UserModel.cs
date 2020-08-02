9494a0a05cf9fc3d1e6382f3fb310d6578e678f1
月半子 王 <616309891@qq.com>
2020-08-02 08:34:34 +0800

    prepare to use

---
+++ ViewModel/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class UserModel
+    {
+        public int AuthorId { set; get; }
+        public string Author { set; get; }
+        public int Level { set; get; }
+    }
+}
