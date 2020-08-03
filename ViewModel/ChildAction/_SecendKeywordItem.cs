ff3798e17bbb41f2a6fa1d29f630d899911b0fd3
月半子 王 <616309891@qq.com>
2020-08-03 11:27:36 +0800

    Prepare to use

---
+++ ViewModel/ChildAction/_SecendKeywordItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.ChildAction
+{
+    public class _SecendKeywordItem
+    {
+        public IList<Item> Items { set; get; }
+    }
+
+    public class Item
+    {
+        public int Id { set; get; }
+        public string SecendKeywordName { set; get; }
+    }
+}
