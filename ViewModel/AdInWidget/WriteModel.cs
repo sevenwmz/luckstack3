56de85e28f6d54be16c1e29dafacfae487a1ea2c
月半子 王 <616309891@qq.com>
2020-08-07 18:15:46 +0800

    Finish [Ad position select] only js and ajax

--- ViewModel/AdInWidget/WriteModel.cs
+++ ViewModel/AdInWidget/WriteModel.cs
@@ -8,5 +8,11 @@ namespace ViewModel.AdInWidget
 {
     public class WriteModel
     {
+        public int Id { set; get; }
+        public string AllSelectDate { set; get; }
+        public bool HasNewAd { set; get; }
+        public int? ChoosAd { set; get; }
+        public string ContentOfAd { set; get; }
+        public string WebSite { set; get; }
     }
 }
