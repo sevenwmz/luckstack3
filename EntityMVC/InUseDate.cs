307408a7764a4b39c81f3a7542f48e4bb143d1f4
月半子 王 <616309891@qq.com>
2020-08-09 11:11:26 +0800

    Prepare to use Ignore

---
+++ EntityMVC/InUseDate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityMVC
+{
+    public class InUseDate
+    {
+        public DateTime UsedDay { set; get; }
+        public int AdPositionId { set; get; }
+        public AdDate AdPosition { set; get; }
+        public int UsedById { set; get; }
+        public User UsedBy { set; get; }
+    }
+}
---
+++ RepositoryMVC/InUseRepository.cs
@@ -0,0 +1,17 @@
+﻿using EntityMVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryMVC
+{
+    public class AdDateRepository : BaceRepository<InUseDate>
+    {
+        public AdDateRepository(SqlContext context) :base(context)
+        {
+
+        }
+    }
+}
